cc6362bb1fa75737c16cbb021e2dddf3263a21a3
sp1der2 <148333159+sp1der2@users.noreply.github.com>
2024-07-03 16:20:59 +0200

    hiding user's password input

--- create_splunk_alerts.py
+++ create_splunk_alerts.py
@@ -1,15 +1,15 @@
-import requests, urllib3, json, os
+import requests, urllib3, json, os, getpass
 import xml.etree.ElementTree as ET
 
-# Informations d'authentification
-username = input("Entrez votre nom d'utilisateur Splunk : ")
-password = input("Entrez votre mot de passe Splunk : ")
+# Credentials & server informations 
+username = input("Enter your Splunk username : ")
+splunk_password = getpass.getpass("Enter your Splunk password : ")
+splunk_base_url=input("Enter your Splunk URL : ")
 
-splunk_base_url=input("Entrez l'URL de votre instance Splunk : ")
-
+# Disable SSL warnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Authentification et récupération du jeton de session
+# Authentication to Splunk API
 auth_url = f"{splunk_base_url}/services/auth/login"
 auth_data = {
     
@@ -19,26 +19,26 @@ auth_data = {
 
 response = requests.post(auth_url, data=auth_data, verify=False)
 
-#Parsing de la réponse HTTP (XML) pour trouver la session_key
+#Parsing HTTP response in order to find the session key
 root = ET.fromstring(response.text)
 session_key = root.find('sessionKey').text
 
+#Define HTTP headers
 headers = {
     'Authorization': f'Splunk {session_key}',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
-
-
+#Function to create alerts and send them to Splunk
 def create_alert():
-    for fichier in os.listdir('./alerts'):
-        with open('./alerts/' + fichier, 'r') as f:
-            alert_actions = json.load(f)
-            response = requests.post(f'{splunk_base_url}/services/saved/searches', headers=headers, data=alert_actions, verify=False)
-            if response.status_code == 201:
-                print('[SUCCESS] L\'alerte a été créée avec succès !')
+    for fichier in os.listdir('./alerts'): # Browsing each file in the alerts folder
+        with open('./alerts/' + fichier, 'r') as f: #Open each alert on RO 
+            alert_actions = json.load(f) # Load the alert in JSON format
+            response = requests.post(f'{splunk_base_url}/services/saved/searches', headers=headers, data=alert_actions, verify=False) #Send POST request to save alert in Splunk
+            if response.status_code == 201: #Check if alert has been successfully created
+                print('[SUCCESS] Alert has been successfully created !')
                 f.close()
             else:
-                print('[FAIL] L\'alerte n\'a pas pu être créée...')
+                print('[FAIL] Alert cannot be created...')
                 f.close()
 
 create_alert()
