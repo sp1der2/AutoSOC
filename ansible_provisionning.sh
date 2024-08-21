539bcccc888bfa2057bccaf681c5edd0b48e88a3
sp1der2 <148333159+sp1der2@users.noreply.github.com>
2024-08-21 16:01:45 +0200

    fixed ansible provisionning

--- ansible_provisionning.sh
+++ ansible_provisionning.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo echo "192.168.56.100 splunk" >> /etc/hosts
-sudo echo "192.168.56.50 client" >> /etc/hosts
-sudo apt-get update && sudo apt-get install unzip pipx -y
-unzip alerts.zip 
-sudo -u vagrant pipx install ansible-core
-export PATH=$PATH:/home/vagrant/.local/bin
-source .bashrc
+echo "192.168.56.100 splunk" >> /etc/hosts
+echo "Adding Splunk Machine to /etc/hosts done"
+echo "192.168.56.50 client" >> /etc/hosts
+echo "Adding client Machine to /etc/hosts done"
+apt update && apt install unzip pipx ansible-core -y
+unzip alerts.zip
 ansible-playbook -i inventory.ini -l splunk splunk.yml
 ansible-playbook -i inventory.ini -l client logs_forwarder.yaml
