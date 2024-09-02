89c1456a7017f9fd8f2f913bbfc7acc81cd6148b
sp1der2 <148333159+sp1der2@users.noreply.github.com>
2024-09-02 13:12:08 +0200

    fixed apt unstable usage

--- ansible_provisionning.sh
+++ ansible_provisionning.sh
@@ -4,7 +4,7 @@ echo "192.168.56.100 splunk" >> /etc/hosts
 echo "Adding Splunk Machine to /etc/hosts done"
 echo "192.168.56.50 client" >> /etc/hosts
 echo "Adding client Machine to /etc/hosts done"
-apt update && apt install unzip pipx ansible-core -y
+apt-get update && apt-get install unzip pipx ansible-core -y
 unzip alerts.zip
 ansible-playbook -i inventory.ini -l splunk splunk.yml
 ansible-playbook -i inventory.ini -l client logs_forwarder.yaml
